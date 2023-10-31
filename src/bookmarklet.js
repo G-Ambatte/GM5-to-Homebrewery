aed1a30441a6c6dc7a116e90988b14a0b2f9a82e
G.Ambatte <sean@robertson-family.nz>
2023-11-01 12:54:41 +1300

    Fix for multiple monsters and missing properties

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -3,76 +3,84 @@ if(location.href=='https://homebrewery.naturalcrit.com/new'){
 	const inputText = prompt('Enter source text:');
 	const regExStr = /<(\S+?)>([\s\S]*?)<\/\1>/gi;
 	const matches = Array.from(inputText.matchAll(regExStr));
-	const output = {};
-	if(matches[0][1].toString()=='monster') {
-		const inputValues = Array.from(matches[0][2].matchAll(regExStr));
-		inputValues.forEach((value)=>{
-			output[value[1]] = output[value[1]] ? [ ...output[value[1]], value[2] ] : [ value[2] ];
-		});
-	}
+	
+	let outputText = '';
 
-	const processData = (data)=>{
-		return data.map((trait)=>{
-			const dataObject= {};
-			const dataArray = Array.from(trait.matchAll(regExStr));
-			dataArray.forEach((dataInfo)=>{ dataObject[dataInfo[1]] = dataInfo[2]; });
-			return dataObject;
-		});
-	};
+	matches.forEach((match)=>{
+		console.log(match);
+		if(match[1].toString()=='monster') {
+			const output = {};
 
-	processItems = ['action','reaction','trait', 'legendary'];
-	processItems.forEach((item)=>{
-		if(output[item]?.length > 0){
-			output[item] = processData(output[item]);
-		};
+			const inputValues = Array.from(match[2].matchAll(regExStr));
+			inputValues.forEach((value)=>{
+				output[value[1]] = output[value[1]] ? [ ...output[value[1]], value[2] ] : [ value[2] ];
+			});
+		
+
+			const processData = (data)=>{
+				return data.map((trait)=>{
+					const dataObject= {};
+					const dataArray = Array.from(trait.matchAll(regExStr));
+					dataArray.forEach((dataInfo)=>{ dataObject[dataInfo[1]] = dataInfo[2]; });
+					return dataObject;
+				});
+			};
+
+			processItems = ['action','reaction','trait', 'legendary'];
+			processItems.forEach((item)=>{
+				if(output[item]?.length > 0){
+					output[item] = processData(output[item]);
+				};
+			});
+
+			const sizeMap = {
+				't' : 'Tiny',
+				's' : 'Small',
+				'm' : 'Medium',
+				'l' : 'Large',
+				'h' : 'Huge',
+				'g' : 'Gargantuan'
+			};
+			output.size[0] = sizeMap[output.size[0]?.toLowerCase()];
+
+			if(outputText.length > 0){ outputText += '\n\\page\n'}
+			let monsterText;
+			try { monsterText = `{{monster,frame,wide\n
+				## ${output.name ? output.name[0] : 'Unnamed Monster'}\n
+				*${output.size ? output.size[0] : 'Variable size'} ${output.type ? output.type[0] : 'unknown'}, ${output.alignment ? output.alignment[0] : 'no known alignment'}*\n
+				___\n
+				**Armor Class** :: ${output.ac ? output.ac[0] : '99'}\n
+				**Hit Points**  :: ${output.hp ? output.hp[0] : '9999'}\n
+				**Speed**       :: ${output.speed ? output.speed[0] : 'none'}\n
+				___\n
+				|  STR  |  DEX  |  CON  |  INT  |  WIS  |  CHA  |\n
+				|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|\n
+				|${output.str ? output.str[0] : '10'}|${output.dex ? output.dex[0] : '10'}|${output.con ? output.con[0] : '10'}|${output.int ? output.int[0] : '10'}|${output.wis ? output.wis[0] : '10'}|${output.cha ? output.cha[0] : '10'}|\n
+				___\n
+				${output.save ? `**Saves** :: ${output.save.join(',')}\n` : ''}
+				${output.skill ? `**Skills** :: ${output.skill.join(',')}\n` : ''}
+				${output.skill || output.save ? '\n:\n' : ''}
+				${output.vulnerable ? `**Damage Vulnerabilities** :: ${output.vulnerable.join(', ')}\n` : ''}
+				${output.resist ? `**Damage Resistances** :: ${output.resist.join(', ')}\n` : ''}
+				${output.immune ? `**Damage Immunities** :: ${output.immune.join(', ')}\n` : ''}
+				${output.conditionImmune ? `**Condition Immunities** :: ${output.conditionImmune.join(', ')}\n` : ''}
+				${output.senses ? `**Senses** :: ${output.senses[0]}` : 'No senses'}, ${output.passive ? `Passive Perception ${output.passive[0]}` : ''}\n
+				${output.langauges ? `**Languages** :: ${output.languages[0]}\n` : ''}
+				${output.cr ? `**Challenge** :: ${output.cr[0]}\n` : ''}
+				___\n
+				${output.trait ? output.trait.map((trait)=>{ return `**${trait.name}.** ${trait.text}`}).join('\n:\n') : ''}
+				${output.reaction ? `:\n#### Reactions\n${output.reaction.map((reaction)=>{ return `**${reaction.name}.** ${reaction.text}`}).join('\n:\n')}` : ''}
+				${output.action ? `\n### Actions\n${output.action.map((action)=>{ return `**${action.name}.** ${action.text}`}).join('\n:\n')}` : ''}
+				${output.legendary ? `\n### Legendary Actions\n${output.legendary.map((legendary)=>{ return `**${legendary.name}.** ${legendary.text}`}).join('\n:\n')}` : ''}
+				\n
+				}}\n
+				`;
+			} catch (err){
+				monsterText = `Failed to process monster\n${err}`;
+			}
+			outputText += monsterText;
+		}
 	});
-
-	const sizeMap = {
-		't' : 'Tiny',
-		's' : 'Small',
-		'm' : 'Medium',
-		'l' : 'Large',
-		'h' : 'Huge',
-		'g' : 'Gargantuan'
-	};
-	output.size[0] = sizeMap[output.size[0].toLowerCase()];
-
-	const outputText = `{{monster,frame,wide\n
-		## ${output.name[0]}\n
-		*${output.size[0]} ${output.type[0]}, ${output.alignment[0]}*\n
-		___\n
-		**Armor Class** :: ${output.ac[0]}\n
-		**Hit Points**  :: ${output.hp[0]}\n
-		**Speed**       :: ${output.speed[0]}\n
-		___\n
-		|  STR  |  DEX  |  CON  |  INT  |  WIS  |  CHA  |\n
-		|:-----:|:-----:|:-----:|:-----:|:-----:|:-----:|\n
-		|${output.str[0]}|${output.dex[0]}|${output.con[0]}|${output.int[0]}|${output.wis[0]}|${output.cha[0]}|\n
-		___\n
-		**Saves** :: ${output.save?.length > 0 ? output.save.join(',') : 'none'}\n
-		**Skills** :: ${output.skill?.length > 0 ? output.skill.join(',') : 'none'}\n
-		:\n
-		**Damage Vulnerabilities** :: ${output.vulnerable?.length > 0 ? output.vulnerable.join(', ') : 'none'}\n
-		**Damage Resistances**     :: ${output.resist?.length > 0 ? output.resist.join(', ') : 'none'}\n
-		**Damage Immunities**      :: ${output.immune?.length > 0 ? output.immune.join(', ') : 'none'}\n
-		**Condition Immunities**   :: ${output.conditionImmune?.length > 0 ? output.conditionImmune.join(', ') : 'none'}\n
-		**Senses**                 :: ${output.senses}, Passive Perception ${output.passive[0]}\n
-		**Languages**              :: ${output.languages[0]}\n
-		**Challenge**              :: ${output.cr[0]}\n
-		___\n
-		${output.trait?.length > 0 ? output.trait.map((trait)=>{ return `**${trait.name}.** ${trait.text}`}).join('\n:\n') : ''}
-		:\n
-		#### Reactions\n
-		${output.reaction?.length > 0 ? output.reaction.map((reaction)=>{ return `**${reaction.name}.** ${reaction.text}`}).join('\n:\n') : ''}
-		\n
-		### Actions\n
-		${output.action?.length > 0 ? output.action.map((action)=>{ return `**${action.name}.** ${action.text}`}).join('\n:\n') : ''}
-		\n
-		### Legendary Actions\n
-		${output.legendary?.length > 0 ? output.legendary.map((legendary)=>{ return `**${legendary.name}.** ${legendary.text}`}).join('\n:\n') : ''}
-		\n
-		}}\n
-		`;
 
 	localStorage.setItem('homebrewery-new', outputText);
 	localStorage.removeItem('homebrewery-new-style');
